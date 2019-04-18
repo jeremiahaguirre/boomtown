913b370f8d61677c6e9768f2cd12cd74ad4b1636
Jeremiah <happytobike@gmail.com>
2019-04-18 16:20:21 -0700

    Populated schema

--- server/api/schema.js
+++ server/api/schema.js
@@ -11,35 +11,52 @@ const { gql } = require('apollo-server-express');
  * We will create the custom Date scalar together.
  */
 module.exports = gql`
-
-  scalar Date
-
   type Item {
-    _: Boolean
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String!
+    itemowner: User!
+    created: String
+    tags: [Tag]
+    borrower: User
   }
 
   type User {
-    _: Boolean
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items: [Item]
+    borrowed: [Item]
   }
 
   type Tag {
-    _: Boolean
+    id: ID!
+    title: String!
   }
 
   type File {
-    _: Boolean
+    id: ID!
+    filename: String!
+    mimetype: String!
+    encoding: String!
+    itemid: ID!
   }
 
   input AssignedTag {
-    _: Boolean
+    id: ID!
+    title: String!
   }
 
   input AssignedBorrower {
-    _: Boolean
+    id: ID!
   }
 
   input NewItemInput {
-    _: Boolean
+    title: String!
+    description: String
+    tags: [AssignedTag]!
   }
 
   type Query {
