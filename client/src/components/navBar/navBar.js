4ae6e9e764a7a0c7d344a02dc189bf408151f8a0
Jeremiah <happytobike@gmail.com>
2019-04-29 15:22:36 -0700

    Nav bar in progress(logo)

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -3,23 +3,37 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import styles from './styles';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 // import MenuIcon from '@material-ui/icons/Menu';
+
+import logo from '../../images/boomtown.svg';
 
 const navBar = ({ classes, props }) => {
   return (
     <div>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton color="inherit" aria-label="Menu" />
-          <Typography variant="h6">News</Typography>
-          <Button>Login</Button>
+        <Toolbar className={classes.navBar}>
+          <div className={classes.logoBtn}>
+            <IconButton color="inherit" aria-label="Menu">
+              <img className={classes.logo} src={logo} />
+            </IconButton>
+          </div>
+
+          <div className={classes.login}>
+            <Button>Share Somthing</Button>
+            <Button>
+              <MoreVertIcon />
+            </Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
   );
 };
 
-export default navBar;
+export default withStyles(styles)(navBar);
---
+++ client/src/components/navBar/styles.js
@@ -0,0 +1,16 @@
+const styles = {
+  logo: {
+    width: '40px',
+    height: 'auto'
+  },
+  navBar: {
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+  login: {
+    textTransform: 'uppercase'
+  }
+};
+export default styles;
