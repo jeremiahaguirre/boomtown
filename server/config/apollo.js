59339bdac147dbc12f0be4ad32e83a92d21dde71
Jeremiah <happytobike@gmail.com>
2019-05-03 13:56:43 -0700

    Server side authentication

--- server/config/apollo.js
+++ server/config/apollo.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+const jwt = require('jsonwebtoken');
+const { AuthDirective } = require('../api/custom-directives');
 
 const typeDefs = require('../api/schema');
 let resolvers = require('../api/resolvers');
@@ -9,29 +11,21 @@ module.exports = ({ app, pgResource }) => {
 
   const schema = makeExecutableSchema({
     typeDefs,
-    resolvers
+    resolvers,
+    schemaDirectives: {
+      auth: AuthDirective
+    }
   });
   const apolloServer = new ApolloServer({
     context: ({ req }) => {
-      // @TODO: Uncomment this later when we add auth (to be added to Apollo's context)
-      // const tokenName = app.get("JWT_COOKIE_NAME")
-      // const token = req ? req.cookies[tokenName] : undefined
-      // -------------------------------
-
+      const tokenName = app.get('JWT_COOKIE_NAME');
+      const encodedToken = req ? req.cookies[tokenName] : undefined;
+      const token = encodedToken
+        ? jwt.decode(encodedToken, app.get('JWT_SECRET'))
+        : undefined;
       return {
-        /**
-         *  Provide Apollo context
-         *
-         * When initializing Apollo, we can provide a context object which will be
-         * passed to each resolver function. This is useful because there are a
-         * number of things we'll need to access in every resolver function.
-         *
-         * Above we can see that we are capturing the cookie from the request object,
-         * and retrieving the token. This is important for authentication.
-         *
-         * Refactor this code and supply any additional information (values, methods, objects...etc)
-         * you'll need to use in your resolving functions.
-         */
+        req,
+        token,
         pgResource
       };
     },
--- server/config/application.js
+++ server/config/application.js
@@ -7,49 +7,29 @@ const path = require('path');
 module.exports = app => {
   const PORT = process.env.PORT || 8080;
 
-  /**
-   *  @TODO: Configuration Variables
-
-   *  JWT_SECRET
-   *
-   *  And the following non-security related information should also be set for use elsewhere:
-   *
-   *  JWT_COOKIE_NAME
-   *  CORS_CONFIG (already set for you below)
-   *
-   *  Use the app.set and process.env to retrieve environment variables, and provide a fallback
-   *  if any are not defined.
-   *
-   *  Use Express' app.set() to store additional configuration information.
-   *
-   *  For example: app.set('PG_HOST', process.env.PG_HOST || 'localhost')
-   */
-
   app.set('PORT', process.env.PORT || 8080);
   app.set('PG_HOST', process.env.PG_HOST || 'localhost');
   app.set('PG_USER', process.env.PG_USER || 'boomtown');
   app.set('PG_PASSWORD', process.env.PG_PASSWORD || 'boomtown');
   app.set('PG_DB', process.env.PG_DB || 'boomtown');
 
+  app.set('JWT_SECRET', process.env.JWT_SECRET || 'keyboardcat');
+  app.set('JWT_COOKIE_NAME', 'bt-token');
+
   app.use(cookieParser());
 
   if (process.env.NODE_ENV === 'production') {
     const root = path.resolve(__dirname, '../public');
-
-    // Serve the static front-end from /public when deployed
     app.use(express.static(root));
     app.use(fallback('index.html', { root }));
   }
 
   if (process.env.NODE_ENV === 'development') {
-    // Allow requests from dev server address
     const corsConfig = {
       origin: 'http://localhost:3000',
       credentials: true
     };
     app.set('CORS_CONFIG', corsConfig);
-
-    // Allow requests from dev server address
     app.use(cors(corsConfig));
   }
 
