81854ac60479980c155ae8c39ded83492a78a981
Jeremiah <happytobike@gmail.com>
2019-05-03 13:57:45 -0700

    Cleaning up code and ToDos

--- server/api/custom-types.js
+++ server/api/custom-types.js
@@ -1,4 +1,5 @@
 const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
 /**
  *  @TODO: Custom Types
@@ -14,8 +15,22 @@ const { GraphQLScalarType } = require('graphql');
  */
 
 // @TOOD: Refactor this into a custom DATE scalar type using new GraphQLScalarType()
-const DateScalar = undefined;
-// -------------------------------
+const DateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  parseValue(value) {
+    return new Date(value); // value from the client
+  },
+  serialize(value) {
+    return value.getTime(); // value sent to the client
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(ast.value); // ast value is always in string format
+    }
+    return null;
+  }
+});
 
 module.exports = {
   DateScalar
